eefd06cdb77b00df737bcc7e4bea0188992ce07f
jtamsut <jtamsut1993@gmail.com>
2018-03-09 16:05:10 -0800

    add update seed file

--- db/seed-challenges.js
+++ db/seed-challenges.js
@@ -13,7 +13,36 @@ const Challenge = require("./../models/Challenge");
 // Seeding
 Challenge.remove({})
   .then(function() {
-    return Challenge.create();
+    return Challenge.create({
+      title: "add",
+      prompt:
+        "Write a function called `add` that accepts *any* quantity of numbers, adds them together and returns the resulting [sum](https://en.wikipedia.org/wiki/Summation). You should assume all parameters will be numbers.",
+      examples: `
+      add(1) //=> 1
+      add(1,50,1.23) //=> 52.23
+      add(7,-12) //=> -5
+      `,
+      due: Date.now(),
+      difficulty: "Basic",
+      assigned: true,
+      tests: [
+        {
+          inputs: [
+            {
+              input: 5
+            },
+            {
+              input: 6
+            }
+          ],
+          outputs: [
+            {
+              output: 11
+            }
+          ]
+        }
+      ]
+    });
   })
   .then(function(challenges) {
     console.log(challenges);
--- models/Test.js
+++ models/Test.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const inputSchema = require("./Input");
 const outputSchema = require("./Output");
 
-const testSchema = mongose.Schema({
+const testSchema = mongoose.Schema({
   inputs: [inputSchema],
   outputs: [outputSchema]
 });
